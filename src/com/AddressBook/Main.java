44dad764e96cbded6487cbb8a25934cc98f23f95
johnsachs1 <johndsachs@gmail.com>
2022-03-29 20:42:52 -0400

    first commit

---
+++ src/com/AddressBook/Main.java
@@ -0,0 +1,18 @@
+package com.AddressBook;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+    private String firstName;
+    private String lastName;
+    private Integer phoneNumber;
+    private String emailAddress;
+
+    public void AddressBook(String firstName, String lastName, Integer phoneNumber, String emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
